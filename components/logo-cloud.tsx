91150784597db886ad8521d710cef1d8ae1d0a01
Chinh Pham <chinhcpdev@gmail.com>
2026-02-02 17:10:00 +0700

    feat: update logo cloud component to use dynamic images and enhance translations for logo section

--- components/logo-cloud.tsx
+++ components/logo-cloud.tsx
@@ -1,17 +1,38 @@
 "use client";
 
-import {
-  Logo01,
-  Logo02,
-  Logo03,
-  Logo04,
-  Logo05,
-  Logo06,
-  Logo07,
-  Logo08,
-} from "@/components/logos";
 import { Marquee } from "@/components/ui/marquee";
 import { useTranslations } from "next-intl";
+import Image from "next/image";
+
+type CompanyLogo = string;
+
+const companyLogos: CompanyLogo[] = [
+  "/companies/1.jpg",
+  "/companies/2.png",
+  "/companies/3.png",
+  "/companies/4.png",
+  "/companies/5.png",
+  "/companies/6.png",
+  "/companies/7.png",
+  "/companies/8.png",
+  "/companies/9.png",
+  "/companies/10.png",
+  "/companies/11.png",
+  "/companies/12.png",
+  "/companies/13.png",
+  "/companies/14.png",
+  "/companies/15.png",
+  "/companies/16.png",
+  "/companies/17.png",
+  "/companies/18.png",
+  "/companies/19.png",
+  "/companies/20.png",
+  "/companies/21.png",
+  "/companies/22.png",
+  "/companies/23.png",
+  "/companies/24.png",
+  "/companies/25.png",
+];
 
 const LogoCloud = () => {
   const t = useTranslations("Home.logoCloud");
@@ -32,30 +53,37 @@ const LogoCloud = () => {
           <div className="space-y-6 sm:space-y-8">
             <Marquee
               pauseOnHover
-              className="[--duration:40s] [&_svg]:mr-8 sm:[&_svg]:mr-10 lg:[&_svg]:mr-12 mask-x-from-70% mask-x-to-90%"
+              className="[--duration:40s] [&_img]:mr-8 sm:[&_img]:mr-10 lg:[&_img]:mr-12 mask-x-from-70% mask-x-to-90%"
             >
-              <Logo01 />
-              <Logo02 />
-              <Logo03 />
-              <Logo04 />
-              <Logo05 />
-              <Logo06 />
-              <Logo07 />
-              <Logo08 />
+              {companyLogos.map((logoSrc, index) => (
+                <Image
+                  key={logoSrc}
+                  src={logoSrc}
+                  alt={`Company logo ${index + 1}`}
+                  width={160}
+                  height={80}
+                  className="h-10 w-auto object-contain opacity-70 transition-opacity hover:opacity-100"
+                />
+              ))}
             </Marquee>
             <Marquee
               pauseOnHover
               reverse
-              className="[--duration:40s] [&_svg]:mr-8 sm:[&_svg]:mr-10 lg:[&_svg]:mr-12 mask-x-from-70% mask-x-to-90%"
+              className="[--duration:40s] [&_img]:mr-8 sm:[&_img]:mr-10 lg:[&_img]:mr-12 mask-x-from-70% mask-x-to-90%"
             >
-              <Logo01 />
-              <Logo02 />
-              <Logo03 />
-              <Logo04 />
-              <Logo05 />
-              <Logo06 />
-              <Logo07 />
-              <Logo08 />
+              {companyLogos
+                .slice()
+                .reverse()
+                .map((logoSrc, index) => (
+                  <Image
+                    key={`${logoSrc}-reverse`}
+                    src={logoSrc}
+                    alt={`Company logo ${companyLogos.length - index}`}
+                    width={160}
+                    height={80}
+                    className="h-10 w-auto object-contain opacity-70 transition-opacity hover:opacity-100"
+                  />
+                ))}
             </Marquee>
           </div>
         </div>
